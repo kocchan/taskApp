92d86b00f57741c836b4f0ffdfd40263e75b46bf
koheiNohara <k.nohara@flux-g.com>
2024-09-24 00:10:24 +0900

    taskapp_v1

--- taskApp/src/App.tsx
+++ taskApp/src/App.tsx
@@ -1,34 +1,91 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [task, setTask] = useState("");
+  const [time, setTime] = useState(0);
+
+  // const initialState = [
+  //   {
+  //   }
+  // ]
+  const [todos, setTodos] = useState([]);
+  const targetTime = 1000;
+
+  const handleNewTask = (e) => {
+    setTask(e.target.value)
+  }
+  const handleNewTime = (event) => {
+    setTime(Number(event.target.value))
+  }
+
+  const handleSubmit = (event) => {
+    event.preventDefault()
+    if (task === "" || time <= 0) {
+      return
+    } else {
+      setTodos(todos => [...todos, { task, time, isCompleted: false }])
+      calculateTotal()
+      setTask("")
+      setTime(0)
+      return
+    }
+  }
+
+  let sTime = 0;
+  let [sumTime, setSumTime] = useState(0);
+
+  const calculateTotal = () => {
+    sTime = sumTime + time
+    setSumTime(sTime)
+  }
+  console.log("todos:", todos)
+
+
+
 
   return (
-    <>
+    <div>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <span>学習内容</span>
+          <input type="text" placeholder='' value={task} onChange={handleNewTask} />
+        </div>
+        <div>
+          <span>学習時間</span>
+          <input type="number" placeholder='0' value={time} onChange={handleNewTime} />
+          <span>時間</span>
+        </div>
+        <div>
+          <span>入力されている学習内容:</span>
+          <span>{task}</span>
+        </div>
+        <div>
+          <span>入力されている時間:</span>
+          <span>{time}</span>
+          <span>時間</span>
+        </div>
+        <button type="submit">登録</button>
+      </form>
+
+      <ul>
+        {todos.length === 0 ? (
+          <li>未入力</li>
+        ) : (
+          todos.map((todo, index) => (
+            <li key={index}>{todo.task}: {todo.time}(時間)</li>
+          ))
+        )}
+      </ul>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <span>合計時間:</span>
+        <span>{sumTime}</span>
+        <span>/{targetTime}(h)</span>
+
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </div>
   )
 }
 
